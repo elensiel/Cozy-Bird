090475f4977672a95e34f3fd0f1c7b8b53ae0564
elensiel <lnclmgn7+github@gmail.com>
2024-12-05 17:02:01 +0800

    almost done

--- script/game_manager.gd
+++ script/game_manager.gd
@@ -2,14 +2,18 @@ extends Node
 class_name GameManager
 
 @onready var label: Label = $Label
-@onready var try := preload("res://scene/retry_panel.tscn")
+@onready var try: PackedScene = preload("res://scene/retry_panel.tscn")
 
-var SAVE_PATH: String = "res://save/score.bin"
+var SAVE_PATH: String = OS.get_user_data_dir() + "/save/score.bin"
 var score: int = 0
 var high_score: int = _load_score()
 
+func inc_score() -> void:
+	score += 1
+	label.text = str(score)
+
 func _load_score() -> int:
-	var DIR: String = "res://save/"
+	var DIR: String = OS.get_user_data_dir() + "/save/"
 	
 	if not DirAccess.dir_exists_absolute(DIR):
 		# creates directory
@@ -21,20 +25,21 @@ func _load_score() -> int:
 	
 	return FileAccess.open(SAVE_PATH, FileAccess.READ).get_16()
 
-func inc_score() -> void:
-	score += 1
-	label.text = str(score)
-
 func save_high_score() -> void:
 	if high_score > score: # if score does not beat pr
 		return
 	
 	# storing high score
 	high_score = score
-	var file = FileAccess.open(SAVE_PATH, FileAccess.READ_WRITE)
-	file.store_16(high_score)
-	file.close()
+	var file = FileAccess.open(SAVE_PATH, FileAccess.WRITE)
+	if file:
+		file.store_16(high_score)
+		file.close()
+	else:
+		print("Error: Could not open file for writing.")
 
 func _on_death_timer_timeout() -> void:
-	var try_temp := try.instantiate()
+	var try_temp: Node = try.instantiate()
 	add_child(try_temp)
+	
+	label.visible = false
