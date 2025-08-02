77a308fa529ff00aa106a4477a33be73343ea307
elensiel <elensiel+github@proton.me>
2025-08-02 16:26:41 +0800

    added audio manager

---
+++ script/gd/audio_manager.gd
@@ -0,0 +1,6 @@
+extends Node
+
+@onready var button_press: AudioStreamPlayer = $ButtonPress
+@onready var caw: AudioStreamPlayer = $Caw
+@onready var flap: AudioStreamPlayer = $Flap
+@onready var score: AudioStreamPlayer = $Score
--- script/gd/main_menu.gd
+++ script/gd/main_menu.gd
@@ -1,20 +1,11 @@
 extends Control
 class_name MainMenu
 
-func _enter_tree() -> void:
-	# put node ref here
-	var background: TextureRect = $TextureRect
-	var panel: PanelContainer = $VBoxContainer/PanelContainer
-	var button_container: VBoxContainer = $VBoxContainer/PanelContainer/CenterContainer/MarginContainer/VBoxContainer
+func _on_play_pressed() -> void:
+	AudioManager.button_press.play()
+
+func _on_quit_pressed() -> void:
+	AudioManager.button_press.play()
+	await AudioManager.button_press.finished
 	
-	var viewport_size: Vector2 = get_viewport().get_visible_rect().size
-	
-	background.custom_minimum_size.x = viewport_size.x * 3
-	background.custom_minimum_size.y = viewport_size.y
-	
-	panel.custom_minimum_size.x = viewport_size.x / 2
-	panel.custom_minimum_size.y = viewport_size.x / 2
-	
-	for button in button_container.get_children():
-		button.custom_minimum_size.x = panel.custom_minimum_size.x / 2
-		button.custom_minimum_size.y = panel.custom_minimum_size.x / 4
+	get_tree().quit()
